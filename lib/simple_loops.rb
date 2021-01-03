584bbd5efe476aa7aba49dbb44afee77dc5f11f4
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-03 23:12:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,29 @@
 # Write your methods here
 
 def loop_message_five_times(message)
-  
-  
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(n, message)
+  counter = 0 
+  while (counter < n) do
+    puts message
+  end
+end
+
+def output_array(array)
+  counter = 0 
+  while (counter < array.size) do
+    puts array[counter]
+  end
+end
+
+def return_string_array(array)
+  counter = 0 
+  string_array []
+  while (counter < array.size) do
+    string_array.push(array[counter].to_stri)
+    
+  string_array
